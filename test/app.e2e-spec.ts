5e71bdfd4e06c546013296ed362f1eae86a9d40a
이승직 <lsjbh45@gmail.com>
2022-09-26 11:38:19 +0000

    #4.2 Testing PATH and DELETE movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -37,7 +37,7 @@ describe('AppController (e2e)', () => {
         .expect([]);
     });
     
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -46,6 +46,18 @@ describe('AppController (e2e)', () => {
           genres: ['test'],
         })
         .expect(201);
+    });
+    
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
     });
     
     it('DELETE', () => {
@@ -68,8 +80,31 @@ describe('AppController (e2e)', () => {
         .expect(404);
     });
     
-    it.todo('DELETE');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
     
-    it.todo('PATCH');
+    it('PATCH 400', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ other: 'thing' })
+        .expect(400);
+    });
+    
+    it('PATCH 404', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/999')
+        .send({})
+        .expect(404);
+    });
+    
+    it('DELETE 200', () => {
+      return request(app.getHttpServer())
+        .delete('/movies/1')
+        .expect(200);
+    });
   });
 });
