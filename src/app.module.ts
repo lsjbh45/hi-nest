1c1985a5ed5f6f2893ce2e26dc8de633fd0c5816
이승직 <lsjbh45@gmail.com>
2022-10-05 16:21:51 +0900

    ftr: database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'hi-nest',
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      synchronize: true,
+    }),
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
