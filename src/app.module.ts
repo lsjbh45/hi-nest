c947c27379bb8b02cd7c6ba572cd8a3b1ab4e9ac
이승직 <lsjbh45@gmail.com>
2022-10-05 16:36:36 +0900

    mod: database configuration에 dotenv library 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'hi-nest',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [`${__dirname}/**/*.entity{.ts,.js}`],
       synchronize: true,
     }),
