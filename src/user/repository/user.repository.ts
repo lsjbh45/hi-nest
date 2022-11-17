febde4681c1eed4440280fe8768fe576238aad00
이승직 <lsjbh45@gmail.com>
2022-11-17 19:05:01 +0900

    Feat: Add refresh token column of user entity

--- src/user/repository/user.repository.ts
+++ src/user/repository/user.repository.ts
@@ -4,6 +4,10 @@ import { CustomRepository } from 'src/database/typeorm-ex.decorator';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
+  async findById(id: number): Promise<User> {
+    return this.findOneBy({ id });
+  }
+
   async findByEmail(email: string): Promise<User> {
     return this.findOneBy({ email });
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,8 +9,27 @@ export class UserService {
     @InjectRepository(UserRepository)
     private readonly userRepository: UserRepository,
   ) {}
+  async findById(id: number): Promise<User> {
+    return this.userRepository.findById(id);
+  }
 
   async findByEmail(email: string): Promise<User> {
     return this.userRepository.findByEmail(email);
+  }
+
+  async setRefreshToken(refreshToken: string, id: number) {
+    await this.userRepository.update(id, { refreshToken });
+  }
+
+  async getUserIfRefreshTokenMatches(refreshToken: string, id: number) {
+    const user = await this.findById(id);
+
+    if (user.refreshToken === refreshToken) {
+      return user;
+    }
+  }
+
+  async removeRefreshToken(id: number) {
+    return this.userRepository.update(id, { refreshToken: null });
   }
 }
