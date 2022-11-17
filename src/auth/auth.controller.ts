33a63abe6f8398dc73ef0a362b6cc8bd8eb6bc86
이승직 <lsjbh45@gmail.com>
2022-11-17 19:06:47 +0900

    Feat: Add refresh and logout api request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,24 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { LocalAuthGuard } from './guard/local-auth.guard';
 import { AuthService } from './auth.service';
 import { LoginInput } from './dto/login.input';
+import { UserService } from 'src/user/user.service';
+import { User } from 'src/user/entity/user.entity';
+import { JwtRefreshGuard } from './guard/jwt-refresh.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
@@ -13,8 +26,42 @@ export class AuthController {
     @Body() _loginInput: LoginInput,
     @Request() req: Request | any,
   ): Promise<any> {
-    const { accessToken, ...accessOption } =
-      await this.authService.getCookieWithJwtAccessToken(req.user);
+    const validatedUser: User = req.user;
+
+    const { accessToken, accessOption } =
+      await this.authService.getCookieWithJwtAccessToken(validatedUser);
+
+    const { refreshToken, refreshOption } =
+      await this.authService.getCookieWithJwtRefreshToken(validatedUser);
+
+    await this.userService.setRefreshToken(refreshToken, validatedUser.id);
+
     req.res.cookie('ACCESS_TOKEN', accessToken, accessOption);
+    req.res.cookie('REFRESH_TOKEN', refreshToken, refreshOption);
+  }
+
+  @UseGuards(JwtRefreshGuard)
+  @Get('refresh')
+  async refresh(@Request() req: Request | any) {
+    const validatedUser: User = req.user;
+
+    const { accessToken, accessOption } =
+      await this.authService.getCookieWithJwtAccessToken(validatedUser);
+
+    req.res.cookie('ACCESS_TOKEN', accessToken, accessOption);
+  }
+
+  @UseGuards(JwtRefreshGuard)
+  @Post('logout')
+  async logOut(@Request() req: Request | any) {
+    const validatedUser: User = req.user;
+
+    const { accessToken, accessOption, refreshToken, refreshOption } =
+      await this.authService.getCookiesForLogOut();
+
+    await this.userService.removeRefreshToken(validatedUser.id);
+
+    req.res.cookie('ACCESS_TOKEN', accessToken, accessOption);
+    req.res.cookie('REFRESH_TOKEN', refreshToken, refreshOption);
   }
 }
